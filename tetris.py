934edd56f8edf8a17c07ddfe068fc0afa09edc23
Raz Matzliah <razm1213@gmail.com>
2021-09-29 22:27:48 +0300

    added draw_next_shape func

--- tetris.py
+++ tetris.py
@@ -205,3 +205,17 @@ def clear_rows(grid, locked):#"Deletes" completed row.
                 newKey = (x,y + inc)
                 locked[newKey] = locked.pop(key)
     return inc
+
+def draw_next_shape(shape, surface):#Shows the next shape on the side of the game grid
+    font = pygame.font.SysFont('comicsans',30)
+    label = font.render('Next shape:', 1, (255,255,255))
+    sx = top_left_x + play_width + 50
+    sy = top_left_y + play_height/2 - 100
+    format = shape.shape[shape.rotation % len(shape.shape)] #Get the original rotated shape (first index)
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                pygame.draw.rect(surface,shape.color,(sx + j*block_size, sy + i*block_size, block_size, block_size), 0)
+
+    surface.blit(label,(sx+ 10, sy - block_size))
